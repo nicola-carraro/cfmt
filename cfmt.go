6ca4ee47951bc1200ea073587d42bb4c9e7a64cd
Nicola Carraro <carraron985@gmail.com>
2023-10-26 22:46:20 +0200

    isSpace() function

--- cfmt.go
+++ cfmt.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 import "os"
+import "unicode/utf8"
+
+type TokenType uint32
+
+const (
+	None TokenType = iota
+	Space
+	Identifier
+	IntConst
+	CharConst
+	FloatConst
+)
+
+type Token struct {
+	Type    TokenType
+	Content string
+}
+
+func isSpace(r rune) bool {
+	return r == ' ' || r == '\t' || r == '\r' || r == '\n' || r == '\v' || r == '\f'
+}
 
 func main() {
 
@@ -11,7 +32,17 @@ func main() {
 
 	if error != nil {
 		fmt.Println("Error reading ", path)
+		os.Exit(1)
 	}
 
-	fmt.Printf("%s", data)
+	//tokens := make([]Token, 100)
+
+	firstRune, _ := utf8.DecodeRune(data)
+
+	if isSpace(firstRune) {
+		fmt.Println("Space")
+	} else {
+		fmt.Println("Not space")
+	}
+
 }
