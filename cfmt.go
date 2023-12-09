fed7b06d114c130ebb7028bb299438fffd09612f
Nicola Carraro <carraron985@gmail.com>
2023-12-09 17:15:10 +0100

    Overwrite input file

--- cfmt.go
+++ cfmt.go
@@ -1369,20 +1369,35 @@ func format(input string) string {
 }
 
 func main() {
+
 	if len(os.Args) < 2 {
 		log.Fatalf("Usage: %s <path>\n", os.Args[0])
 	}
 
-	path := os.Args[1]
-	data, err := os.ReadFile(path)
+	for i := 1; i < len(os.Args); i++ {
+		path := os.Args[i]
+		
+		data, err := os.ReadFile(path)
 
-	if err != nil {
-		log.Fatalf("Error reading %s: %s", path, err)
+		if err != nil {
+			log.Fatalf("Error reading %s: %s", path, err)
+		}
+
+
+		text := fmt.Sprintf("%s", data)
+
+		formattedText := format(text)
+
+		os.WriteFile(path, []byte(formattedText), 0600)	
+
+		if err != nil {
+			log.Fatalf("Error writing %s: %s", path, err)
+		}
+
 	}
 
-	text := fmt.Sprintf("%s", data)
 
-	formattedText := format(text)
 
-	fmt.Print(formattedText)
+
+
 }
